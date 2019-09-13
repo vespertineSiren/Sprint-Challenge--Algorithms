f504a780b2bf3e59370c108d5a1cca1f2c758263
Patrick Martin <patrickmartin88@gmail.com>
2019-09-13 13:33:16 -0700

    passing th.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,6 +7,10 @@ Your function must utilize recursion. It cannot contain any loops.
 
 def count_th(word):
     count = 0
+    if len(word) < 2:
+        return count
+
     if "th" == word[0:2]:
-        count = 1 + count_th(word[1:])
-    return count
+        return 1 + count_th(word[1:])
+    else:
+        return count_th(word[1:])
